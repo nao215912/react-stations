61452ba66412582201a2685e94dee4917d7d585f
nao215912 <naoki3.14159@gmail.com>
2022-04-17 10:43:43 +0900

    sta11

--- src/BreedsSelect.js
+++ src/BreedsSelect.js
@@ -1,9 +1,9 @@
 import * as React from "react";
 
-export const BreedsSelect = ({breeds}) => {
+export const BreedsSelect = ({breeds, onChangeSelectedBreed, selectedBreed}) => {
   return (<>
-      <select size="1" name="dog">
-          {breeds.map((breed) => <option value="dog">{breed}</option>)}
+      <select value={selectedBreed} onChange={(e) => onChangeSelectedBreed(e)}>
+          {breeds.map((breed) => <option key={breed} value={breed}>{breed}</option>)}
       </select>
       </>)
 }
--- src/DogListContainer.js
+++ src/DogListContainer.js
@@ -4,6 +4,8 @@ import {BreedsSelect} from "./BreedsSelect";
 
 export const DogListContainer = () => {
   const [breeds, setBreeds] =useState([])
+    const [selectedBreed, setSelectedBreed] = useState("african")
+    const [breedURLs, setBreedURLs] = useState([])
     useEffect(() => {
       fetch("https://dog.ceo/api/breeds/list/all")
           .then(res => res.json())
@@ -16,9 +18,23 @@ export const DogListContainer = () => {
               }
           )
     },[])
-
+    const onChangeSelectedBreed = (e) => {
+        setSelectedBreed(e.target.value)
+        fetch(`https://dog.ceo/api/breed/${selectedBreed}/images/random/12`)
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    setBreedURLs(result.message)
+                },
+                (err) => {
+                    console.error(err)
+                }
+            )
+    }
     return (    <>
-        <BreedsSelect breeds={breeds}/>
+        <BreedsSelect breeds={breeds} onChangeSelectedBreed={onChangeSelectedBreed} selectedBreed={selectedBreed}/>
+            <p>{selectedBreed}</p>
+            {breedURLs.map((breedURL) => <img src={breedURL} alt={`犬の画像`}/>)}
         </>
     )
 }
